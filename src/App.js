52075fc68017e2ebf5c450bad9c756f8f3d2ca06
Nita Constantin <ctinnita88@gmail.com>
2024-08-28 19:13:41 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HsTestimonials from "./hsTestimonials/HsTestimonials";
+
+function App() {
+  return <HsTestimonials />;
+}
+
+export default App;
---
+++ src/hsTestimonials/HsTestimonials.jsx
@@ -0,0 +1,52 @@
+import "./hsTestimonials.scss";
+
+function HsTestimonials() {
+  return (
+    <div className="hsTestimonialsWrapper">
+      <div className="container hsTestimonials-container">
+        <div className="row">
+          <div className="col-12 hsTestimonials-item">
+            <div className="hsTestimonials-item-img item-img-1">
+              <div className="hsTestimonials-item-overlay">
+                <div className="hsTestimonials-content">
+                  <div className="hsTestimonialsc-text">
+                    “Great place and service. Raffaele is great guy, does my
+                    hair a very month and I have to admit, my hair is hard to
+                    handle but he does an awesome job.” — Moises N
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 hsTestimonials-item">
+            <div className="hsTestimonials-item-img item-img-2">
+              <div className="hsTestimonials-item-overlay">
+                <div className="hsTestimonials-content">
+                  <div className="hsTestimonialsc-text">
+                    “Great experience at Raffaele’s Hair Studio, wonderful
+                    customer service, friendly people and amazing styles!” —
+                    Henri Smith
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 hsTestimonials-item">
+            <div className="hsTestimonials-item-img item-img-3">
+              <div className="hsTestimonials-item-overlay">
+                <div className="hsTestimonials-content">
+                  <div className="hsTestimonialsc-text">
+                    “Great experience at Raffaele’s Hair Studio, wonderful
+                    customer service, friendly people and amazing styles!” —
+                    Carolina N.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HsTestimonials;
